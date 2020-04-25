d1c90688cdb89567573b4bd9a18783c960d13138
Hamid <halaeiv@gmail.com>
2020-04-25 19:38:33 +0430

    dont escape \.

--- src/Illuminate/Validation/Validator.php
+++ src/Illuminate/Validation/Validator.php
@@ -288,8 +288,6 @@ class Validator implements ValidatorContract
         // rule. Any error messages will be added to the containers with each of
         // the other error messages, returning true if we don't have messages.
         foreach ($this->rules as $attribute => $rules) {
-            $attribute = str_replace('\.', '->', $attribute);
-
             if ($this->shouldBeExcluded($attribute)) {
                 $this->removeAttribute($attribute);
 
